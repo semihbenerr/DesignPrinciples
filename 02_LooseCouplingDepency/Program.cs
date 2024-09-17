b171275178439cbb71f7d8b21cf78519de641e68
Semih BENER <benersemih2434@gmail.com>
2024-09-17 15:21:26 +0300

    Loose Coupling Depency

---
+++ 02_LooseCouplingDepency/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("");
+interface IMailServer
+{
+    void Send(string to, string body);
+}
+
+class MailSender
+{
+    public void Send(IMailServer mailServer)
+    {
+        mailServer.Send("to", "body");
+    }
+
+}
+class Hotmail : IMailServer
+{
+    public void Send(string to, string body)
+    {
+        throw new NotImplementedException();
+    }
+}
+class Gmail : IMailServer
+{
+    public void Send(string to, string body)
+    {
+        throw new NotImplementedException();
+    }
+}
+class Yandex : IMailServer
+{
+    public void Send(string to, string body)
+    {
+        throw new NotImplementedException();
+    }
+}
