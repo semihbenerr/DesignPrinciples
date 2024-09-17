753d461b485dff6c32c86e4806368340e9439c96
Semih BENER <benersemih2434@gmail.com>
2024-09-17 15:20:28 +0300

    Tight Coupling Depency

---
+++ 01_TightCouplingDepency/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("");
+class MailSender
+{
+    public void Send()
+    {
+        Gmail gmail = new();//aşağıdaki açıklama satırları kalkarsa hata verecek ve bu bir sıkı bağımlılık yani en MailSender Gmail classına bağımlı ve  bu en ufak değişiklikte bile bize sürekli hata verecektir.
+        gmail.Send();
+    }
+}
+class Gmail
+{
+    public Gmail(/*string f*/)
+    {
+
+    }
+    public void Send(/*string to*/)
+    {
+
+    }
+}
