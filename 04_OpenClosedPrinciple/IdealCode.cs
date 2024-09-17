5dc127da5029bd5a00d16d206e7d916bec95cdbc
Semih BENER <benersemih2434@gmail.com>
2024-09-17 15:23:37 +0300

    Open Closed Principle

---
+++ 04_OpenClosedPrinciple/IdealCode.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_OpenClosedPrinciple.IdealCode;
+
+class ParaGonderici
+{
+    void Gonder(IBanka banka, int tutar, string hesapNo)
+    {
+        banka.ParaTransferi(tutar, hesapNo);
+    }
+}
+interface IBanka
+{
+    bool ParaTransferi(int tutar, string hesapNo);
+}
+class Garanti : IBanka
+{
+    public string HesapNo { get; set; }
+    void ParaGonder(int tutar)
+    {
+    }
+    public bool ParaTransferi(int tutar, string hesapNo)
+    {
+        try
+        {
+            HesapNo = hesapNo;
+            ParaGonder(tutar);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
+class Halkbank : IBanka
+{
+    void GonderilecekHesapNo(string hesapNo)
+    {
+    }
+    void Gonder(int tutar)
+    {
+    }
+    public bool ParaTransferi(int tutar, string hesapNo)
+    {
+        try
+        {
+            GonderilecekHesapNo(hesapNo);
+            Gonder(tutar);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
---
+++ 04_OpenClosedPrinciple/NotIdealCode.cs
@@ -0,0 +1,39 @@
+﻿using _04_OpenClosedPrinciple.IdealCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_OpenClosedPrinciple.NotIdealCode;
+
+class ParaGonderici
+{
+    void Gonder(int tutar)
+    {
+        Garanti garanti = new();
+        garanti.HesapNo = "123";
+        garanti.ParaGonder(tutar);
+
+        Halkbank halkbank = new();
+        halkbank.GonderilecekHesapNo("123");
+        halkbank.Gonder(tutar);
+    }
+}
+class Garanti
+{
+    public string HesapNo { get; set; }
+    public void ParaGonder(int tutar)
+    {
+    }
+}
+class Halkbank
+{
+    public void GonderilecekHesapNo(string hesapNo)
+    {
+    }
+    public void Gonder(int tutar)
+    {
+    }
+}
+
