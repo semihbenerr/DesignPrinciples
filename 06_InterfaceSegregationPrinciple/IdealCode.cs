06f642a1d075e15ac01e209278b8aa775a0266e7
Semih BENER <benersemih2434@gmail.com>
2024-09-17 15:27:18 +0300

    InterfaceSegregationPrinciple

---
+++ 06_InterfaceSegregationPrinciple/IdealCode.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_InterfaceSegregationPrinciple.IdealCode;
+
+interface IScan
+{
+    void Scan();
+}
+interface IFax
+{
+    void Fax();
+}
+interface IPrint
+{
+    void Print();
+}
+interface IPrintDuplex
+{
+    void PrintDuplex();
+}
+
+class SamsungPrinter : IScan, IFax, IPrint, IPrintDuplex
+{
+    public void Fax()
+    {
+        Console.WriteLine("Fax işlemleri");
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Print işlemleri");
+    }
+
+    public void PrintDuplex()
+    {
+        Console.WriteLine("PrintDuplex işlemleri");
+    }
+
+    public void Scan()
+    {
+        Console.WriteLine("Scan işlemleri");
+    }
+}
+
+class HPPrinter : IScan, IFax
+{
+    public void Fax()
+    {
+        Console.WriteLine("Fax işlemleri");
+    }
+
+    public void Scan()
+    {
+        Console.WriteLine("Scan işlemleri");
+    }
+}
+
+class LexmarkPrinter : IScan, IPrintDuplex
+{
+    public void PrintDuplex()
+    {
+        Console.WriteLine("PrintDuplex işlemleri");
+    }
+
+    public void Scan()
+    {
+        Console.WriteLine("Scan işlemleri");
+    }
+}
---
+++ 06_InterfaceSegregationPrinciple/NotIdealCode.cs
@@ -0,0 +1,87 @@
+﻿using _06_InterfaceSegregationPrinciple.IdealCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_InterfaceSegregationPrinciple.NotIdealCode;
+
+interface IPrintter
+{
+    void Fax();
+    void Print();
+    void PrintDuplex();
+    void Scan();
+}
+
+class SamsungPrinter : IPrintter
+{
+    public void Fax()
+    {
+        Console.WriteLine("Fax işlemleri");
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Print işlemleri");
+    }
+
+    public void PrintDuplex()
+    {
+        Console.WriteLine("PrintDuplex işlemleri");
+    }
+
+    public void Scan()
+    {
+        Console.WriteLine("Scan işlemleri");
+    }
+}
+
+class HPPrinter: IPrintter
+{
+    public void Fax()
+    {
+        Console.WriteLine("Fax işlemleri");
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Print işlemleri");
+    }
+
+    public void PrintDuplex()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Scan()
+    {
+        Console.WriteLine("Scan işlemleri");
+
+    }
+}
+
+class LexmarkPrinter : IPrintter
+{
+    public void Fax()
+    {
+        Console.WriteLine("Fax işlemleri");
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Print işlemleri");
+
+    }
+
+    public void PrintDuplex()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Scan()
+    {
+        throw new NotImplementedException();
+    }
+}
